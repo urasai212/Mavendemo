3b0ed1b7062c78dafcd77ce0cd3103da5dac2509
urasai212 <ec2-user@ip-172-31-38-42.us-east-2.compute.internal>
2019-02-14 13:51:06 +0000

    first commit

---
+++ src/main/java/cloudseals/com/App.java
@@ -0,0 +1,13 @@
+package cloudseals.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
